572b168fb436cc9105248e978cfb74af5592de52
rahel-yab <rahelyabebal@gmail.com>
2026-03-18 09:19:57 +0300

    added reverse proxy

--- JobGateway.Api/Program.cs
+++ JobGateway.Api/Program.cs
@@ -1,6 +1,14 @@
+using Yarp.ReverseProxy;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// 1. Add YARP services and tell it to load config from appsettings.json
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// 2. Map the proxy to the request pipeline
+app.MapReverseProxy();
 
 app.Run();
